1bbabece01f8e89a806bb61d11ae2557ab9b8c64
Dalius Butrimas <36799822+DaliusB@users.noreply.github.com>
2018-03-01 16:39:57 +0200

    v0.0

---
+++ BazesGynybosZaidimas/Assets/Scripts/Enemy.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+	public int EnemyHealth = 100; // enemy health points.
+	public bool isDead = false; // bool to check if enemy is dead.
+	public bool damageTaken = false; // bool to check if enemy got damaged.
+
+
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void Movement()
+	{
+
+	}
+	// To Do: EnemyHealth is reduced dependig on what tipe of gun he got shot at.
+	public void Health()
+	{
+
+
+	}
+
+}
---
+++ BazesGynybosZaidimas/Assets/Scripts/Player.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+	public int PlayerHealth = 100; // player health points.
+	public bool isDead = false; // bool to check if player is dead.
+	public bool damageTaken = false; // bool to check if player got damaged.
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
