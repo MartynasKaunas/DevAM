acb5079507cff93f524330d46e94486cfd041f8a
AurimasKavaliauskas <36799371+AurimasKavaliauskas@users.noreply.github.com>
2018-03-03 21:24:43 +0200

    Padariau HP
    https://app.vivifyscrum.com/boards/20496/SMPR-8
    https://app.vivifyscrum.com/boards/20496/SMPR-16
    https://app.vivifyscrum.com/boards/20496/SMPR-11
    https://app.vivifyscrum.com/boards/20496/SMPR-47

--- BazesGynybosZaidimas/Assets/Scripts/Enemy.cs
+++ BazesGynybosZaidimas/Assets/Scripts/Enemy.cs
@@ -1,21 +1,44 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.UI;
 using UnityEngine;
+
 
 public class Enemy : MonoBehaviour {
 	//Too access variables from different scripts variable should be changed to static
+     public int enemySpeed = 1; // How fast enemy is moving
+    public float curent_enemy_hp=10;
+  //  public float start_hp;
+    public int enemy_HP = 10;
+    public int count_deths_this_enemy;
+    public Image HP;
+    void OnCollisionEnter2D (Collision2D col)
+    {
+        if(col.gameObject.tag == "Bullet")
+        {
+            curent_enemy_hp -= 1;
+            if (curent_enemy_hp <= 0)
+            {
+                count_deths_this_enemy ++;
+                Destroy(gameObject);
+            }
+        }
 
-	public int enemySpeed = 1; // How fast enemy is moving
+    }
+    void HealthBar() {
+        HP.fillAmount = curent_enemy_hp / enemy_HP;
+    }
+	
 
 
 	// Use this for initialization
 	void Start () {
 
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
-
+        HealthBar();
 		Movement (); 
 		                                                                    
 	}
--- BazesGynybosZaidimas/Assets/Scripts/Player.cs
+++ BazesGynybosZaidimas/Assets/Scripts/Player.cs
@@ -1,10 +1,29 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.UI;
 using UnityEngine;
 
 public class Player : MonoBehaviour {
+    public float curent_player_hp = 3;
+    public int player_HP = 3;
+    public Image HP;
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.name == "Enemy")
+        {
+            curent_player_hp -= 1;
+            if (curent_player_hp == 0)
+            {
+                
+                Destroy(gameObject);
+            }
+        }
 
-
+    }
+    void HealthBar()
+    {
+        HP.fillAmount = curent_player_hp / player_HP;
+    }
     // Use this for initialization
     void Start()
     {
@@ -14,7 +33,7 @@ public class Player : MonoBehaviour {
     // Update is called once per frame
     void Update()
     {
-        
+        HealthBar();
     }
 
 
