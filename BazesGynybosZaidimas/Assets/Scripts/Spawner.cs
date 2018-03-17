ba684029a43bdff1d8756051eb49bae24127dd9b
AurimasKavaliauskas <36799371+AurimasKavaliauskas@users.noreply.github.com>
2018-03-17 21:57:56 +0200

    #SMPR-67 #SMPR-10 eina skirtingais keliais, o ne vienas paskui kitą. Kas keletą sekundžių turi atsirasti nauji priešai
    https://app.vivifyscrum.com/boards/20496/SMPR-67
    https://app.vivifyscrum.com/boards/20496/SMPR-10

--- BazesGynybosZaidimas/Assets/Scripts/Spawner.cs
+++ BazesGynybosZaidimas/Assets/Scripts/Spawner.cs
@@ -7,16 +7,34 @@ public class Spawner : MonoBehaviour
     bool isSpawning = false;
     public float minTime = 3.0f;
     public float maxTime = 8.0f;
-    public GameObject[] enemies;  // Array of enemy prefabs.
+    //public GameObject[] enemies;  // Array of enemy prefabs.
     public GameObject enemyPrefab;
-
+    public GameObject enemyPrefab1;
     IEnumerator SpawnObject(float seconds)
     {
         Debug.Log("Waiting for " + seconds + " seconds");
 
         yield return new WaitForSeconds(seconds);
-        Instantiate(enemyPrefab, transform.position, transform.rotation);
+      
+        Vector3 V;
+        V = new Vector3(transform.position.x, Random.Range(-3.0f, 2.5f));
+        //   Random randomSp = new Random();
 
+        int i = Random.Range(1,3);
+        Debug.Log("iiiiiiiiiiiiiiiiii" + i + " seconds");
+        switch (i)
+        {
+            case 1:
+               Instantiate(enemyPrefab1, V, transform.rotation);
+                break;
+            case 2:
+                Instantiate(enemyPrefab, V, transform.rotation);
+                break;
+        }
+        
+        
+
+    
         //We've spawned, so now we could start another spawn     
         isSpawning = false;
     }
@@ -28,6 +46,9 @@ public class Spawner : MonoBehaviour
         {
             isSpawning = true; //Yep, we're going to spawn
             StartCoroutine(SpawnObject(Random.Range(minTime, maxTime)));
+
         }
     }
+
+
 }
