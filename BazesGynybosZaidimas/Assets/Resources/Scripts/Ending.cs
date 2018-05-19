933fe2a44b70a9e30a0eaee43c3b04d5527d1d6d
Martynas Kaunas <36590078+MartynasKaunas@users.noreply.github.com>
2018-05-19 15:44:16 +0300

    SMPR-114
    https://app.vivifyscrum.com/boards/20496/SMPR-114

---
+++ BazesGynybosZaidimas/Assets/Resources/Scripts/Ending.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Ending : MonoBehaviour {
+
+    public Text endReport;
+    public static bool GameOver = false;
+
+    public AudioClip GameOverMusic;
+    public AudioSource SpawnerAudioSource;
+    private AudioSource audioSource;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+        endReport.text = "";
+    }
+
+    public void EndMePlz()
+    {
+        SpawnerAudioSource.Stop();
+        audioSource.PlayOneShot(GameOverMusic);
+        endReport.text = "GAME OVER" /*ゲーム　オーワ"*/ + System.Environment.NewLine + " You shot " + Enemy.count_deaths_this_enemy + " Enemies";
+        GameOver = true;
+    }
+
+    public void Freeze()
+    {
+        if (Time.timeScale == 0)
+            Time.timeScale = 1;
+        else
+            Time.timeScale = 0;
+
+    }
+
+    public void NextLevel()
+    {
+        Freeze();
+        FindObjectOfType<Spawner>().LevelUpSpawner();
+    }
+
+    void Update()
+    {
+
+        if (Input.GetKeyDown("escape"))
+        {
+            if (GameOver == true)
+            {
+                
+                //iš naujo pradedant žaidimą reikia visus statsus gražint į pradines reikšmes, gal yra kažkoks lengvesnis būdas
+                Player.player_HP = 5;
+                Player.current_player_HP = 5;
+                Player.current_player_MP = 50;
+                Player.player_MP = 100;
+                Player.maxBulletCount = 20;
+                Player.bulletCount = 20;
+                Player.score = 0;
+                Taikymasis.weaponType = 1;
+
+                endReport.text = "";
+                shopMenu.levelClear = false;
+                shopMenu.shotgunBought = false;
+                shopMenu.cannonBought = false;
+
+                Spawner.level = 1;
+                Spawner.leftToSpawn = 0;
+                Spawner.currentlyAlive = 3;
+
+                SceneManager.LoadScene(0);
+                
+                GameOver = false;
+                
+            }
+            else Application.Quit();
+        }         
+        if (Input.GetKeyDown("p"))
+            Freeze();
+    }
+}
+
---
+++ BazesGynybosZaidimas/Assets/test2345.cs
@@ -0,0 +1,86 @@
+﻿using UnityEngine;
+using UnityEngine.TestTools;
+using NUnit.Framework;
+using System.Collections;
+
+public class test2345 {
+
+    [UnityTest]
+    public IEnumerator ParticleSelfDestructTest()
+    {
+
+        var particlePrefab = Resources.Load("Prefabs/particles/BasicGunShotParticle");
+
+        Vector3 V = new Vector3(0, Random.Range(-3.0f, 2.0f));
+        Quaternion rotation = Quaternion.Euler(0, 0, Mathf.Atan2(0, 0) * Mathf.Rad2Deg);
+
+        GameObject particle = GameObject.Instantiate(particlePrefab, V, rotation) as GameObject;
+
+        yield return new WaitForSeconds(2.5f);
+
+        Assert.IsTrue(particle == null);
+    }
+
+    [UnityTest]
+    public IEnumerator EnemySpawnTest()
+    {
+
+        var enemyPrefab = Resources.Load("Prefabs/Slow");
+
+        Vector3 V = new Vector3(0, Random.Range(-3.0f, 2.0f));
+        Quaternion rotation = Quaternion.Euler(0, 0, Mathf.Atan2(0, 0) * Mathf.Rad2Deg);
+
+        GameObject enemySlow = Transform.Instantiate(enemyPrefab, V, rotation) as GameObject;
+
+        Assert.AreEqual(enemySlow.tag, "Enemy");
+
+        yield return null;
+    }
+
+    [UnityTest]
+    public IEnumerator EnemyDeathTest()
+    {
+        Player.score = 0;
+
+        var enemyPrefab = Resources.Load("Prefabs/Slow");
+
+        Vector3 V = new Vector3(0, Random.Range(-3.0f, 2.0f));
+        Quaternion rotation = Quaternion.Euler(0, 0, Mathf.Atan2(0, 0) * Mathf.Rad2Deg);
+
+        GameObject enemySlow = Transform.Instantiate(enemyPrefab, V, rotation) as GameObject;
+        Enemy e1 = enemySlow.GetComponent<Enemy>();
+
+        e1.curent_enemy_hp = 0;
+
+        yield return new WaitForSeconds(2f);
+
+        Assert.IsTrue(enemySlow == null);
+        Assert.IsTrue(Player.score > 0);
+
+    }
+
+    [UnityTest]
+    public IEnumerator SpawnerTest()
+    {
+
+        var spawnerPrefab = Resources.Load("Prefabs/Spawner");
+
+        Vector3 V = new Vector3(0, Random.Range(-3.0f, 2.0f));
+        Quaternion rotation = Quaternion.Euler(0, 0, Mathf.Atan2(0, 0) * Mathf.Rad2Deg);
+
+        GameObject spawner = Transform.Instantiate(spawnerPrefab, V, rotation) as GameObject;
+        Spawner p1 = spawner.GetComponent<Spawner>();
+
+        int enemyCount1 = GameObject.FindGameObjectsWithTag("Enemy").Length;
+
+        p1.minTime = 1f;
+        p1.maxTime = 2f;
+        p1.LevelUpSpawner();
+
+        yield return new WaitForSeconds(10f);
+
+        int enemyCount2 = GameObject.FindGameObjectsWithTag("Enemy").Length;
+
+        Assert.IsTrue(enemyCount1 < enemyCount2);
+    }
+}
